2ff1780507567955515a14d8aa3b0e0fda58e57b
Sokmesa KHIEV <ksokmesa>
2015-01-05 14:08:21 +0700

    change deploy code

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :group, 'ilab'
 set :deploy_via, :remote_cache
 set :branch, `hg branch`.strip
 
-server '192.168.1.95', :app, :web, :db, primary: true
+server '54.169.173.164', :app, :web, :db, primary: true
 
 default_run_options[:pty] = true
 default_environment['TERM'] = ENV['TERM']
