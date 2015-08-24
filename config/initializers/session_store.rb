08a18ede40c4c9b28530ac31dd1db6ca918fc00a
thyda_eng <thyda@instedd.org>
2015-08-24 10:48:21 +0700

    fix session conflict for wfp and wfp_lite

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ResourceMap::Application.config.session_store :cookie_store, key: '_resource_map_session'
+ResourceMap::Application.config.session_store :cookie_store, key: '_resource_map_wfp_lite_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
