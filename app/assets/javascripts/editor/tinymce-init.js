580a4b342cd758ba0a8052780d0a3ba01557e54f
thyda_eng <thyda@instedd.org>
2016-05-06 11:18:23 +0700

    embed khmer font to editor

--- app/assets/javascripts/editor/tinymce-init.js
+++ app/assets/javascripts/editor/tinymce-init.js
@@ -1,8 +1,52 @@
 $(document).ready(function() {
+	var fontLists = "(ខ្មែរ)Khmer=khmer;"+
+									"(ខ្មែរ)Khmer Moul=moul;"+
+									"Andale Mono=andale mono,times;"+
+									"Arial=arial,helvetica,sans-serif;"+
+									"Arial Black=arial black,avant garde;"+
+									"Book Antiqua=book_antiquaregular,palatino;"+
+									"Corda Light=CordaLight,sans-serif;"+
+									"Courier New=courier_newregular,courier;"+
+									"Flexo Caps=FlexoCapsDEMORegular;"+
+									"Lucida Console=lucida_consoleregular,courier;"+
+									"Georgia=georgia,palatino;"+
+									"Helvetica=helvetica;"+
+									"Impact=impactregular,chicago;"+
+									"Museo Slab=MuseoSlab500Regular,sans-serif;"+
+									"Museo Sans=MuseoSans500Regular,sans-serif;"+
+									"Oblik Bold=OblikBoldRegular;"+
+									"Sofia Pro Light=SofiaProLightRegular;"+
+									"Symbol=webfontregular;"+
+									"Tahoma=tahoma,arial,helvetica,sans-serif;"+
+									"Terminal=terminal,monaco;"+
+									"Tikal Sans Medium=TikalSansMediumMedium;"+
+									"Times New Roman=times new roman,times;"+
+									"Trebuchet MS=trebuchet ms,geneva;"+
+									"Verdana=verdana,geneva;"+
+									"Webdings=webdings;"+
+									"Wingdings=wingdings,zapf dingbats"+
+									"Aclonica=Aclonica, sans-serif;"+
+									"Michroma=Michroma;"+
+									"Paytone One=Paytone One, sans-serif;"+
+									"Andalus=andalusregular, sans-serif;"+
+									"Arabic Style=b_arabic_styleregular, sans-serif;"+
+									"Andalus=andalusregular, sans-serif;"+
+									"KACST_1=kacstoneregular, sans-serif;"+
+									"Mothanna=mothannaregular, sans-serif;"+
+									"Nastaliq=irannastaliqregular, sans-serif";
+
+
+
+
+
+
 	tinyMCE.init({
 		width: 980,
 		selector: 'textarea.custom-tinymce',
-		plugins: [ 'table advlist autolink lists anchor', 'fullscreen', 'insertdatetime table contextmenu paste code pagebreak'],
-		toolbar: "undo redo | alignleft aligncenter alignright bold italic underline styleselect table code fullscreen pagebreak"
+		plugins: [ 'preview print media image table advlist autolink lists anchor',
+							'fullscreen', 'insertdatetime table contextmenu paste code pagebreak'],
+		toolbar: ["undo redo | alignleft aligncenter alignright bold italic underline styleselect " +
+							" fontselect fontsizeselect table code fullscreen pagebreak"],
+		font_formats:  fontLists,
 	})
 });
