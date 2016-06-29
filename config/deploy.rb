aeb5de80d717844e398258979743c6c1bc4b0666
sokmesakhiev <mesa@instedd.org>
2016-06-30 04:52:08 +0700

    change deploy to use recaptchar and telmetry in share and fix some security leak

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ namespace :deploy do
   end
 
   task :symlink_configs, :roles => :app do
-    %W(settings.yml google_maps.key nuntium.yml aws.yml).each do |file|
+    %W(settings.yml google_maps.key nuntium.yml aws.yml telemetry.yml recaptcha.yml).each do |file|
       run "ln -nfs #{shared_path}/#{file} #{release_path}/config/"
     end
   end
