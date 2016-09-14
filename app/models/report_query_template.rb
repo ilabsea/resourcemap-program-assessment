9991d2db5952f3899b510876a9ce516f98088ea3
thyda_eng <thyda@instedd.org>
2016-09-14 16:17:51 +0700

    rendering report query with template

--- app/models/report_query_template.rb
+++ app/models/report_query_template.rb
@@ -1,7 +1,42 @@
+# == Schema Information
+#
+# Table name: report_query_templates
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  template        :text
+#  collection_id   :integer
+#  report_query_id :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  uuid            :string(255)
+#
+
 class ReportQueryTemplate < ActiveRecord::Base
+  REPORT_PLACE_HOLDER = "{report}"
+
   belongs_to :collection
   belongs_to :report_query
   attr_accessible :name, :template, :report_query_id
 
   validates :name, :template, presence: true
+
+  before_create :generate_uuid
+
+  def generate_uuid
+    self.uuid = UUIDTools::UUID.timestamp_create.to_s
+  end
+
+  def has_report_place_holder?
+    template.include?(ReportQueryTemplate::REPORT_PLACE_HOLDER)
+  end
+
+  def translate_template(translation)
+    template.gsub(ReportQueryTemplate::REPORT_PLACE_HOLDER, translation )
+  end
+
+  def report_result
+    @report_result ||= ReportQuerySearch.new(report_query).query
+    @report_result
+  end
 end
--- spec/models/report_query_template_spec.rb
+++ spec/models/report_query_template_spec.rb
@@ -1,5 +1,53 @@
+# == Schema Information
+#
+# Table name: report_query_templates
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  template        :text
+#  collection_id   :integer
+#  report_query_id :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  uuid            :string(255)
+#
+
 require 'spec_helper'
 
 describe ReportQueryTemplate do
+  describe '#has_report_place_holder' do
+    context "template contains report place holder" do
+      it "return true" do
+        template = ReportQueryTemplate.make(template: 'test report {report}')
+        expect(template.has_report_place_holder?).to eq true
+      end
+    end
 
+    context 'template does not contain report place holder' do
+      it "return false" do
+        template = ReportQueryTemplate.make(template: 'test report')
+        expect(template.has_report_place_holder?).to eq false
+      end
+
+    end
+  end
+
+  describe '#translate_template' do
+    context "template contains report place holder" do
+      it "return a string translated by the translation " do
+        template = ReportQueryTemplate.make(template: "test report 1.{report} \n 2.{report}")
+        result = template.translate_template("<h1>result</h1>")
+        expect(result).to eq "test report 1.<h1>result</h1> \n 2.<h1>result</h1>"
+      end
+    end
+
+    context "template does not contain report place holder" do
+      it "return a string translated by the translation " do
+        template = ReportQueryTemplate.make(template: 'test report}')
+        result = template.translate_template("<h1>result</h1>")
+        expect(result).to eq "test report}"
+      end
+    end
+
+  end
 end
