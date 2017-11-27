19fe5ac90c6b1fc025bbb82b02e7e25c1cf62177
thyda_eng <thyda@instedd.org>
2017-11-27 14:11:43 +0700

    finish api alert notification

--- plugins/alerts/controllers/api/v1/thresholds_controller.rb
+++ plugins/alerts/controllers/api/v1/thresholds_controller.rb
@@ -1,15 +1,8 @@
 module Api::V1
   class ThresholdsController < ApplicationController
-
-    def index
-      new_thresholds = []
-      thresholds.all.each do |threshold|
-        new_thresholds << threshold if threshold.email_notification[:to_reporter] == 'true'
-      end
-
-      respond_to do |format|
-        format.json { render json: new_thresholds }
-      end
+    def to_reporter
+      result = thresholds.select{|t| t.email_notification["to_reporter"] == 'true' }
+      render json: result
     end
 
   end
