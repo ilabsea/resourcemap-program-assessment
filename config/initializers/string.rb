ee21e4b431d3fd0d764d087df105c2c89a1b02ba
thyda_eng <thyda@instedd.org>
2017-12-28 15:27:35 +0700

    parse field code to empty string in alert message notification when no value

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -10,6 +10,7 @@ class String
 
   def render_template_string(option_hash)
     self.gsub(/\[[\w\s()]+\]/) do |template|
+      has_value = true
       option_hash.each do |key, value|
         if template == '['+ key+ ']'
           if key == "Site Name"
@@ -17,9 +18,13 @@ class String
           else
             template = value
           end
+          has_value = true
           break
+        else
+          has_value = false
         end
       end
+      template = '' if has_value == false
       template
     end
   end
